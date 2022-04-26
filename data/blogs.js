d055f5efd56dbf185c4f90c11e777cca77d4cd91
Nikita Shinde <shindenikita111998@gmail.com>
2022-04-26 16:48:48 +0530

    new blog data added

--- data/blogs.js
+++ data/blogs.js
@@ -16,6 +16,14 @@ const blogs = [
         Sed viverra mauris lacus, eu feugiat nulla maximus vel.Vivamus sed vulputate elit.Curabitur auctor venenatis rutrum.Ut vulputate vitae eros vel placerat.Aenean semper nulla vel justo vulputate, nec eleifend odio ultrices.Nunc semper feugiat sem quis ultricies.Praesent tristique molestie lorem, id interdum tellus pellentesque non.Donec at diam sit amet leo commodo viverra eu at urna.In semper tincidunt augue ac vehicula.Duis efficitur in lorem sit amet accumsan.Donec pretium accumsan dignissim.Nunc at malesuada neque.`
     },
     {
+        slug: "AngularJs",
+        title: "AngularJs Blog",
+        description: "Learning AngularJs",
+        blog: `Cras nec facilisis urna, vitae mattis lorem. Phasellus quis sapien lacus. In hac habitasse platea dictumst. Ut quis pellentesque neque, ac finibus mauris. Nullam dignissim aliquet luctus. Phasellus id vestibulum nisl, sed imperdiet quam. Suspendisse consectetur odio nec blandit mollis. Praesent lobortis leo felis, at lobortis mi laoreet ut. Nullam id arcu dui. Fusce ornare dictum aliquet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar, sapien quis facilisis cursus, turpis arcu euismod ipsum, eu sodales velit augue nec nibh. Sed viverra lorem in fermentum cursus. Nulla bibendum luctus metus ut laoreet. Aenean quis aliquam leo, eu vulputate nulla.
+
+        Sed viverra mauris lacus, eu feugiat nulla maximus vel.Vivamus sed vulputate elit.Curabitur auctor venenatis rutrum.Ut vulputate vitae eros vel placerat.Aenean semper nulla vel justo vulputate, nec eleifend odio ultrices.Nunc semper feugiat sem quis ultricies.Praesent tristique molestie lorem, id interdum tellus pellentesque non.Donec at diam sit amet leo commodo viverra eu at urna.In semper tincidunt augue ac vehicula.Duis efficitur in lorem sit amet accumsan.Donec pretium accumsan dignissim.Nunc at malesuada neque.`
+    },
+    {
         slug: "ReactJs",
         title: "ReactJs Blog",
         description: "Learning ReactJs",
